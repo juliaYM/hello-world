4c2e0265e5f13c89eea80f50c7f149138882148c
SJ <SJ@DESKTOP-3ESHQPK>
2016-04-14 14:51:57 +0800

    added one line in HelloWorld

--- src/com/test/HelloWorld.java
+++ src/com/test/HelloWorld.java
@@ -1,10 +1,13 @@
 package com.test;
 
+import javax.swing.SortingFocusTraversalPolicy;
+
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello world!");
+		System.out.println("modified line");
 	}
 
 }
