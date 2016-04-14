39bc50992a0766d83443433d8e81cd297afc6e5d
SJ <SJ@DESKTOP-3ESHQPK>
2016-04-14 16:28:34 +0800

    deleted import statement and added the "thrid line"

--- src/com/test/HelloWorld.java
+++ src/com/test/HelloWorld.java
@@ -1,13 +1,12 @@
 package com.test;
 
-import javax.swing.SortingFocusTraversalPolicy;
-
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello world!");
 		System.out.println("modified line");
+		System.out.println("third line");
 	}
 
 }
