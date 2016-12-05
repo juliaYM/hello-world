ecf5c30fbe61572c466c68a0fc0f6b341c3d9b85
juliamyin(殷铭) <juliamyin@tencent.com>
2016-12-05 17:26:05 +0800

    add feature1

--- src/com/test/HelloWorld.java
+++ src/com/test/HelloWorld.java
@@ -11,6 +11,7 @@ public class HelloWorld {
 		System.out.println("fifth line");
 		System.out.println("sixth line");
 		System.out.println("added this line on 20161205");
+		System.out.println("add feature1 on 20151205");
 	}
 
 }
